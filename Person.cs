39e90dd89b2583bdd82955b6eb5c2c66587dcfa3
ThaiTuan123 <53283080+ThaiTuan123@users.noreply.github.com>
2020-03-31 09:31:44 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName,int age, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary} leva.";
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age >= 30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percentage / 200;
+            }
+        }
+    }
+}
